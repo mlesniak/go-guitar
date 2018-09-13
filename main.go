975eb182c0f92f08d2b1169c90ee6e1e46228c23
Michael Lesniak <mlesniak@micromata.de>
2018-09-13 05:42:31 +0200

    Add keyboard quit event handler

--- main.go
+++ main.go
@@ -8,10 +8,22 @@ import (
 	"os/signal"
 )
 
+// Global constants for visualization
+const width = 800
+const height = 800
+
+// Global constants for audio recording
+const bufferSize = 1024
+const sampleRate = 88200
+
 func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
 	fmt.Println("Starting recording, press CTRL+C to abort.")
+
+	// Initialize audio library.
+	portaudio.Initialize()
+	defer portaudio.Terminate()
 
 	// Initialize visualization library.
 	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
@@ -19,12 +31,8 @@ func main() {
 	}
 	defer sdl.Quit()
 
-	// Initialize audio library.
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-
-	window, err := sdl.CreateWindow("go guitar", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
+	window, err := sdl.CreateWindow("Go guitar", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		width, height, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
@@ -37,15 +45,15 @@ func main() {
 	defer renderer.Destroy()
 
 	// Create buffer for sampling data and open the stream.
-	in := make([]int32, 1024)
-	stream, err := portaudio.OpenDefaultStream(1, 0, 88200, len(in), in)
+	in := make([]int32, bufferSize)
+	stream, err := portaudio.OpenDefaultStream(1, 0, sampleRate, len(in), in)
 	check(err)
 	defer stream.Close()
 
 	check(stream.Start())
-	f := 800 / float32(len(in))
+	f := width / float32(len(in))
 	amplit := 500000000
-	g := 600 / float32(amplit)
+	g := height / float32(amplit)
 	for {
 		check(stream.Read())
 
@@ -55,16 +63,22 @@ func main() {
 		renderer.SetDrawColor(255, 0, 0, 255)
 		for i := range in {
 			x := int(float32(i) * f)
-			y := int32(float32(in[x])*g + 300)
+			y := int32(float32(in[x])*g + height/2)
 			renderer.DrawPoint(int32(x), y)
 		}
 		renderer.Present()
 
 		// See https://stackoverflow.com/questions/39637824/border-titlebar-not-properly-displaying-in-sdl-osx
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
+		var event sdl.Event
+		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch t := event.(type) {
 			case *sdl.QuitEvent:
 				return
+			case *sdl.KeyboardEvent:
+				if t.Keysym.Sym == 27 {
+					// Quit on Escape key.
+					return
+				}
 			}
 		}
 
